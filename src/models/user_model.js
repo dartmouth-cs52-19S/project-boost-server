0bc56cd84835dfb84149257a6202defe5fb8077f
Robert He <robert.he.19@dartmouth.edu>
2019-05-15 19:59:16 -0400

    added location model and separated unnecessary information from user_model

--- src/models/user_model.js
+++ src/models/user_model.js
@@ -4,35 +4,12 @@ const bcrypt = require('bcryptjs');
 
 const UserSchema = new Schema({
   _id: { type: String, unique: true }, // taken from the uID in firebase
-  locationAlgorithmOutput: [
-    {
-      latlongLocation: String,
-      values: [{
-        startTime: String,
-        endTime: String,
-      }],
-    },
-  ], // stores the output of the algorithm
   presetProductiveLocations: [String], // on initial start, user selects which places are productive or not
   homeLocation: String, // for every user, have a "home location" that they enter
   latlongHomeLocation: String, // convert "home location" into a lat long e.g. "43.7041448 , -72.2890539"
-  userInputOnProductivity: [{ // e.g. {date: May 12 2019, time: 4:00PM, location: Topliff, productivity: 1}
-    date: String,
-    startTime: String,
-    endTime: String,
-    location: String,
-    latlonglocation: String,
-    productivity: Number,
-  }],
-  settings: [{
+  settings: [{ // e.g. [ {name: backgroundColor, value: red}, { name: lang, value: FR}, { name: nightMode value: true} ]
     name: String,
     value: String,
-  }],
-  lastSevenDays: [{
-    type: Schema.Types.Mixed,
-  }],
-  lastThirtyDays: [{
-    type: Schema.Types.Mixed,
   }],
 });
 
